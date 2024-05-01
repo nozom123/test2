253f859f81bca903cdd99a9ea2f10a80ee3d34ae
nozom123 <167749298+nozom123@users.noreply.github.com>
2024-05-02 01:26:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const fs = require("fs");
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");

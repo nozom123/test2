5f5c45e90c13e1b10b5fb1bb481af250870265e5
nozom123 <167749298+nozom123@users.noreply.github.com>
2024-05-02 01:33:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.use(
+  cors({
+    origin: "https://9b9366-8e.myshopify.com",
+  })
+);
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
